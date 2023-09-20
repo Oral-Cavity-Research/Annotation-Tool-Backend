3f637ab7f6d68b9c78e798a8f7dbd2a32e375593
SashiniLiyanage <e17190@eng.pdn.ac.lk>
2023-09-20 16:46:02 +0530

    change naigation sorting

--- routes/image.js
+++ routes/image.js
@@ -312,12 +312,12 @@ router.get("/data/:id", authenticateToken, async (req, res) => {
         const image = await Image.findById(req.params.id,{})
         .populate("last_comment", "action")
 
-        const prevDocuments = await Image.find({ status: image.status, _id: { $lt: image._id } },"_id")
+        const prevDocuments = await Image.find({ status: image.status, updatedAt: { $gt: image.updatedAt } },"_id")
         .sort({ updatedAt : -1 })
         .limit(1); 
 
-        const nextDocuments = await Image.find({ status: image.status, _id: { $gt: image._id } },"_id")
-        .sort({ updatedAt: 1 })
+        const nextDocuments = await Image.find({ status: image.status, updatedAt: { $lt: image.updatedAt } },"_id")
+        .sort({ updatedAt: -1 })
         .limit(1);
 
         const prevDocument = prevDocuments.length > 0? prevDocuments[0]._id: null
@@ -339,12 +339,12 @@ router.get("/navigation/:id/:status", authenticateToken, async (req, res) => {
     try {
         const image = await Image.findById(req.params.id,{})
 
-        const prevDocuments = await Image.find({ status: req.params.status, _id: { $lt: image._id } },"_id")
+        const prevDocuments = await Image.find({ status: req.params.status, updatedAt: { $gt: image.updatedAt } },"_id")
         .sort({ updatedAt : -1 })
         .limit(1); 
 
-        const nextDocuments = await Image.find({ status: req.params.status, _id: { $gt: image._id } },"_id")
-        .sort({ updatedAt: 1 })
+        const nextDocuments = await Image.find({ status: req.params.status, updatedAt: { $lt: image.updatedAt } },"_id")
+        .sort({ updatedAt: -1 })
         .limit(1);
 
         const prevDocument = prevDocuments.length > 0? prevDocuments[0]._id: null
